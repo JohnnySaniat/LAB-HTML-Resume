72659355d1e408a0738db43b1326fb20654e4ac2
Johnny Saniat <Jcsaniat@gmail.com>
2023-07-13 15:40:05 -0500

    added a Hire Me button that creates a pop-up box

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Resume")
+
+const hireBtnText = document.getElementById("hirebtn-text");
+const hireBtn = document.getElementById("hirebtn");
+
+hireBtn.addEventListener("click", () => {
+  hireBtnText.classList.remove("hidden");
+  hireBtnText.classList.add("popup-message");
+});
+
+hireBtnText.addEventListener("animationend", () => {
+  hireBtnText.classList.remove("popup-message");
+  hireBtnText.classList.add("hidden");
+});
